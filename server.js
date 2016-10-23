6f0429584a997d6e4075cb4b88d558c50b754321
amalmohan98 <amalmohan611@gmail.com>
2016-10-23 19:27:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/ui/madi.png', function (req, res) {
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
-}};
+});
 var canvas1 = document.getElementById('canvas1');
 var canvas2 = document.getElementById('canvas2');
 var canvas3 = document.getElementById('canvas3');
