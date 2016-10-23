a521e4064d111b9a86e9bc30a284faa08f70049a
amalmohan98 <amalmohan611@gmail.com>
2016-10-23 20:12:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -196,3 +196,7 @@ function animloop() {
   requestAnimationFrame(animloop);
 }
 animloop();
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
